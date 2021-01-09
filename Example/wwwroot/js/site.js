4dbba52b6790c6164e7ab88acab6a24247e3d0c9
dany1468 <dany1468@gmail.com>
2021-01-09 23:56:45 +0900

    Example ES6

--- Example/wwwroot/js/site.js
+++ Example/wwwroot/js/site.js
@@ -1,7 +1,10 @@
-﻿"use strict";
-
-// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+'use strict';
+
+$(function () {
+    $('h1').text('Welcome ES6 and Sass');
+});
 
